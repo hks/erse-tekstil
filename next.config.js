7212579dee99b176cb2c7d03ce6ee6ac4d48f690
hks <himmet@pembepanjur.com>
2023-03-22 20:40:53 +0300

    Eslint is disabled

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
     fiber: false,
     includePaths: [path.join(__dirname, 'styles')],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
